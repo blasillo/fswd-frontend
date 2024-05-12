f4126938b8a6dc2a13ee7efc46aca38b06f6453c
unknown <blasillous@gmail.com>
2024-05-13 00:17:59 +0200

    estetica

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {TareaService} from "./servicios/tarea.service";
 import {provideHttpClient} from "@angular/common/http";
+import {ConfirmationService} from "primeng/api";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(),ConfirmationService,
     TareaService]
 
 };
